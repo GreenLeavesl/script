8a5da803241eaa27cc144d1233a1d00c7a3854d4
GreenLeavesl <1615033252lu@gmail.com>
2022-02-16 16:01:46 +0800

    script push again

--- .history/iconpy2_20210927153652.py
+++
@@ -1,6 +0,0 @@
-import mmh3
-import requests
-response = requests.get('https://www.xxxx.com/favicon.ico')
-favicon = response.content.encode('base64')
-hash = mmh3.hash(favicon)
-print 'http.favicon.hash:'+str(hash)
